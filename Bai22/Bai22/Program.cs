58850f4be979e7903d542615c06ad63fd479113a
AnNguyen <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-10-19 14:30:46 +0700

    Bai 22

---
+++ Bai22/Bai22/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Bai22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Bai 22");
+
+            Console.Write("Nhap so tien : ");
+            double money = double.Parse(Console.ReadLine());
+            Console.Write("\nNhap lai suat : ");
+            double rate = double.Parse(Console.ReadLine());
+
+            Console.Write("Tinh theo de quy: ");
+            monney = money;
+            Console.WriteLine(calMonth2(2*money, rate));
+
+            Console.Write("Tinh khong de quy: ");
+            Console.WriteLine(calMonth2(money, rate));
+        }
+
+        /*
+         * Using Recursion
+         */
+        public static double monney;
+        public static int month;
+        static int calMonth(double money, double rate)
+        {
+            if(money <= monney)
+            {
+                return month;
+            }
+            month++;
+            return calMonth(money - money * rate / 100, rate);
+        }
+
+        /*
+         * No Using Recursion
+         */
+        static int calMonth2(double money, double rate)
+        {
+            int month = 0;
+            double m = money;
+            while(m <= 2 * money)
+            {
+                m = m + m * rate / 100;
+                month++;
+            }
+            return month;
+        }
+    }
+}
